569539f2e781c70ba1d8b6b45d3fe4b3dc7aad32
anna-moore <ammoore434@gmail.com>
2021-03-08 15:49:48 -0600

    phase1: create and display new team member

--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ namespace PlanYourHeist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan your Heist!");
+
+            TeamMember newUser = new TeamMember()
+            {
+                Name = TeamMember.UserName(),
+                SkillLevel = TeamMember.UserSkillLevel(),
+                CourageFactor = TeamMember.UserCourageFactor(),
+            };
+
+            newUser.DisplayMemberInfo();
+
         }
     }
 }
--- TeamMember.cs
+++ TeamMember.cs
@@ -7,5 +7,41 @@ namespace PlanYourHeist
         public string Name { get; set; }
         public int SkillLevel { get; set; }
         public double CourageFactor { get; set; }
+
+        public static string UserName()
+        {
+            Console.Write("Enter your name: ");
+            string newMember = Console.ReadLine();
+
+            return newMember;
+        }
+
+        //use static because calling a method in the team member class without a team member existing yet ch20
+        //add if and else statements to ensure that user input means requirements
+        public static int UserSkillLevel()
+        {
+            Console.Write("Enter a positive interger for your skill level: ");
+            int skill = Int32.Parse(Console.ReadLine());
+
+            return skill;
+        }
+
+        public static double UserCourageFactor()
+        {
+            Console.Write("Enter a number 0.0 - 2.0 for your courage level: ");
+            double courage = Double.Parse(Console.ReadLine());
+
+            return courage;
+        }
+
+        public void DisplayMemberInfo()
+        {
+
+            Console.WriteLine(Name);
+            Console.WriteLine($"This is user's skill level {SkillLevel}");
+            Console.WriteLine($"This is user courage level {CourageFactor}");
+
+        }
+
     }
 }
