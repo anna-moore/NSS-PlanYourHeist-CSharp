9b9f3deaf73b4648ecb78d5d7676c31b88df8c17
anna-moore <ammoore434@gmail.com>
2021-03-16 13:14:19 -0500

    IRobber interface

---
+++ Robber.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace PlanYourHeist
+{
+    public interface IRobber
+    {
+        string Name { get; set; }
+        int SkillLevel { get; set; }
+        int PercentageCut { get; set; }
+        void PerformSkill();
+    }
+}
+
+// A string property for Name
+// An integer property for SkillLevel
+// An integer property for PercentageCut
+// A method called PerformSkill that takes in a Bank parameter and doesn't return anything.
