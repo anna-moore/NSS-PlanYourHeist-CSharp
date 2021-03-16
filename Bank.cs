b8e034af561ab23d6859c163a8aad0811a776d2b
anna-moore <ammoore434@gmail.com>
2021-03-16 12:04:23 -0500

    back class

---
+++ Bank.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace PlanYourHeist
+{
+    public class Bank
+    {
+        public int CashOnHand { get; set; }
+        public int AlarmSound { get; set; }
+        public int VaultScore { get; set; }
+        public int SecurityGuardScore { get; set; }
+
+        public bool IsSecure
+        {
+            get
+            {
+
+                if (CashOnHand <= 0 && AlarmSound <= 0 && VaultScore <= 0 && SecurityGuardScore <= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
